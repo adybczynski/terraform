026e06e8638b101878c4e99c60d183b54088b7ea
adyb <adyb@DESKTOP-M8BGQP4.localdomain>
2021-10-20 16:14:30 +0200

    Terraform init

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-058e6df85cfc7760b"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyLearning"
+  }
+}
+
